2a17a899d057dddd7e7f67f3c4f7b1164af16755
shekher <36192685+shekhercse@users.noreply.github.com>
2022-09-01 01:02:01 +0530

    sidecart

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,8 +4,9 @@ import logo from '../public/logo.png';
 import Link from 'next/link';
 import { useRef } from 'react';
 import {
-  AiOutlineShoppingCart, AiFillCloseCircle
+  AiOutlineShoppingCart, AiFillCloseCircle, AiFillMinusCircle, AiFillPlusCircle
 } from 'react-icons/ai';
+import {BsFillBagCheckFill} from 'react-icons/bs';
 
 
 
@@ -20,53 +21,70 @@ const Navbar = () => {
     else if (!ref.current.classList.contains('translate-x-full')) {
       ref.current.classList.remove('translate-x-0')
       ref.current.classList.add('translate-x-full')
-  
-  
-    }}
-    const ref = useRef()
- 
-    return (
-      <div>
-        <header className="text-gray-600 body-font">
-          <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center shadow">
-            <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-              <Link href='/'><Image
-                src={logo}
-                alt="Picture of the author"
-                width={100}
-                height={100}
-              /></Link>
 
-              <span className="ml-3 text-xl">Ecommerce</span>
-            </a>
-            <nav className="md:ml-auto font-bold flex flex-wrap items-center text-base justify-center">
-              <Link href='/electronics'><a className="mr-5 hover:text-gray-900">Electronics</a></Link>
-              <Link href={'/clothing'}><a className="mr-5 hover:text-gray-900">Clothing</a></Link>
 
-              <Link href={'/computing'}><a className="mr-5 hover:text-gray-900">Computing</a></Link>
-              <Link href={'/digitalservices'}><a className="mr-5 hover:text-gray-900">Digital Services</a></Link>
-              <Link href={'/blogs'}><a className="mr-5 hover:text-gray-900">Blogs</a></Link>
-              <Link href={'/electronics'}><a className="mr-5 hover:text-gray-900">Contact Us</a></Link>
-            </nav>
-            <div onClick={toggleCart}>
+    }
+  }
+  const ref = useRef()
 
-              <AiOutlineShoppingCart className="text-2xl" />
-            </div>
-            <div ref={ref} className="sidecart absolute top-1 right-0 bg-pink-400 p-10 transition-transform translate-x-full transform">
-              <h2 className="font-bold text-xl">
-                Shopping Cart
-              </h2>
-              <span onClick={toggleCart} className="absolute top-5 right-2 cursor-pointer text-2xl text-pink-600">
-                <AiFillCloseCircle />
-              </span>
-              <ul>
-                <li><span> Electronics</span></li>
-              </ul>
-            </div>
+  return (
+    <div>
+      <header className="text-gray-600 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center shadow">
+          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <Link href='/'><Image
+              src={logo}
+              alt="Picture of the author"
+              width={50}
+              height={50}
+            /></Link>
+
+            <span className="ml-3 text-xl">Ecommerce</span>
+          </a>
+          <nav className="md:ml-auto font-bold flex flex-wrap items-center text-base justify-center">
+            <Link href='/electronics'><a className="mr-5 hover:text-gray-900">Electronics</a></Link>
+            <Link href={'/clothing'}><a className="mr-5 hover:text-gray-900">Clothing</a></Link>
+
+            <Link href={'/computing'}><a className="mr-5 hover:text-gray-900">Computing</a></Link>
+            <Link href={'/digitalservices'}><a className="mr-5 hover:text-gray-900">Digital Services</a></Link>
+            <Link href={'/blogs'}><a className="mr-5 hover:text-gray-900">Blogs</a></Link>
+            <Link href={'/electronics'}><a className="mr-5 hover:text-gray-900">Contact Us</a></Link>
+          </nav>
+          <div onClick={toggleCart}>
+
+            <AiOutlineShoppingCart className="text-2xl" />
           </div>
-        </header>
-      </div>
-    );
-  };
+          <div ref={ref} className=" w-100 h-full sidecart absolute top-1 right-0 bg-pink-100 p-10 transition-transform translate-x-full transform">
+            <h2 className="font-bold text-xl">
+              Shopping Cart
+            </h2>
+            <span onClick={toggleCart} className="absolute top-5 right-2 cursor-pointer text-2xl text-pink-600">
+              <AiFillCloseCircle />
+            </span>
+            <ol className="font-semibold list-decimal">
+              <li>
+                <div className="item flex my-5">
+                  <div className="w-2/3 font-semibold"> Electronics </div>
+                  <div className="flex font-semibold items-center jsutify-center w-1/3 text-lg"><AiFillMinusCircle className="cursor-pointer text-pink-500" /><span className="mx-2 text-sm">1</span> <AiFillPlusCircle className="cursor-pointer text-pink-500" /></div>
+                </div>
+              </li>
+              <li>
+                <div className="item flex my-5">
+                  <div className="w-2/3 font-semibold"> Electronics </div>
+                  <div className="flex font-semibold items-center jsutify-center w-1/3 text-lg"><AiFillMinusCircle className="cursor-pointer text-pink-500" /><span className="mx-2 text-sm">1</span> <AiFillPlusCircle className="cursor-pointer text-pink-500" /></div>
+                </div>
+              </li>
+            </ol>
+            <div className='flex'>
+            <button className="flex m-1 text-white bg-pink-300 border-0 py-1 px-1 focus:outline-none hover:bg-pink-600 rounded text-sm"><BsFillBagCheckFill className=" m-1"/> Checkout</button>
+            <button className="flex m-1 text-white bg-pink-300 border-0 py-1 px-1 focus:outline-none hover:bg-pink-600 rounded text-sm">Clear cart</button>
+            </div>
 
-  export default Navbar
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar
