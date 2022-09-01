f42f959f8cad3c0d413cc477dc4523a6a5b43c35
shekher <36192685+shekhercse@users.noreply.github.com>
2022-09-01 23:10:42 +0530

    product page

---
+++ pages/api/pincode.js
@@ -0,0 +1,6 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+    res.status(200).json([ 220510, 175032, 175015, 175001 ])
+  }
+  
