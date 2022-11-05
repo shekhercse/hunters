5ab579de9dabdaa48c30927b2242ec3f30ef5607
shekher <36192685+shekhercse@users.noreply.github.com>
2022-11-05 22:34:04 +0530

    api update test

--- pages/signup.js
+++ pages/signup.js
@@ -30,7 +30,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault()
     const data = { name, email, password, confirmPassword }
-    let res = await fetch('http://localhost:3000/api/signup', {
+    let res = await fetch('http://www.hunterstore.tk/api/signup', {
       method: 'POST', // or 'PUT'
       headers: {
         'Content-Type': 'application/json'
